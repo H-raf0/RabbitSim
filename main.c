6ad267ad9adaa465e2c4c728530059340fbb50f9
Achraf El Allali <Achraf.EL_ALLALI@etu.uca.fr>
2023-03-27 00:25:57 +0200

    we can give birth and add new generations to the old ones

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int fibonnaci(int N) {
 }
 
 typedef struct Rabbit {
-    char sexe;            // M: Male, F: Female
+    char sex;            // M: Male, F: Female
     int status;           // 0:dead ou 1:alive
     int age;              // 0:just born, -1 dead? (age in months)
     int mature;           // 0:no, 1:yes(adult)
@@ -64,7 +64,7 @@ void updateStats(Srabbit* rabbit) {
 int giveBirth(Srabbit*  rabbit) {
     int prctg = 25;
     
-    if (rabbit->sexe == 'F' && rabbit->mature == 1 && rabbit->pregnant == 0) {
+    if (rabbit->sex == 'F' && rabbit->mature == 1 && rabbit->pregnant == 0) {
         if (rabbit->nbLitters < rabbit->nbLittersY) {
 
             rabbit->nbLitters += 1;
@@ -82,27 +82,59 @@ int giveBirth(Srabbit*  rabbit) {
     }
 }
 
-void createRabbitsList();
+char generate_sex() {
+    return (genrand_real1() * 100 <= 50) ? 'M' : 'F';
+}
+
+Srabbit* createRabbitsList(int nb, Srabbit** head) {
+    
+    Srabbit *rabbit = malloc(sizeof(Srabbit*));
+    Srabbit *nextRabbit = rabbit;
+
+    *rabbit = (Srabbit){ generate_sex(), 1, 0, 0, 0, 0, 35, NULL};
+    for (int i = 0; i < nb - 1; i++) {
+
+        nextRabbit = nextRabbit->nextRabbit;
+        nextRabbit = &(Srabbit) { generate_sex(), 1, 0, 0, 0, 0, 35, NULL };
+    }
+
+    (*head)->nextRabbit = rabbit; // first one in the list
+    return nextRabbit; // last one created
+}
 
 void sim(int N) {  //number of months
     Srabbit* rabbit = malloc(sizeof(Srabbit*));
+    Srabbit* currentRabbit = malloc(sizeof(Srabbit*));
+    Srabbit* tempRabbit = malloc(sizeof(Srabbit*));
+    Srabbit* headListRabbit = malloc(sizeof(Srabbit*));
     int population = 2;
     int newKittens;
 
     //first 2 rubbits
     *rabbit = (Srabbit){ 'M', 1, 0, 0, 0, 0, 35, &(Srabbit) { 'F', 1, 0, 0, 0, 0, 35, NULL } };
+    //to save the adress of 'rabbit' since it is the first rabbit ever created
+    currentRabbit = rabbit;
 
+    //temporary rabbit used to connect new generations to the old ones
+    *tempRabbit = (Srabbit){ '?', 0, 0, 0, 0, 0, 0, NULL};
+    //head of the new generation linked list
+    headListRabbit = tempRabbit->nextRabbit;
+    
+
+    //life cycle
     for (int i = 0; i < N; i += TIME_STEP) {
         //run of all the existing rabbits
         for (int j = 0; j < population; j++) {
             if (rabbit->status != 0) {  // if not already dead
 
-                newKittens = giveBirth(rabbit);
                 updateStats(rabbit);
+                population += (newKittens = giveBirth(rabbit));
+                tempRabbit->nextRabbit = createRabbitsList(newKittens, tempRabbit);
 
-                rabbit = rabbit->nextRabbit;
+                if(j != population - 1) rabbit = rabbit->nextRabbit; //to avoid getting last pointer which is equal to NULL
             }
         }
+
     }
 
 }
