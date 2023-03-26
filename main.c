78e35075496fe0334b40c94a7640388cce922111
Achraf El Allali <Achraf.EL_ALLALI@etu.uca.fr>
2023-03-26 22:56:58 +0200

    adding giveBirth function and some small modifications

--- main.c
+++ main.c
@@ -21,12 +21,15 @@ int fibonnaci(int N) {
 }
 
 typedef struct Rabbit {
-    char sexe;  // M: Male, F: Female
-    int status; //  0:dead ou 1:alive
-    int age;    // 0:just born, -1 dead? (age in months)
-    int mature; // 0:no, 1:yes(adult)
+    char sexe;            // M: Male, F: Female
+    int status;           // 0:dead ou 1:alive
+    int age;              // 0:just born, -1 dead? (age in months)
+    int mature;           // 0:no, 1:yes(adult)
+    int pregnant;         // 0:no, 1:yes
+    int nbLittersY;       // kittens must have in a year
+    int nbLitters;        // kittens made so far
     unsigned int srvRate; // survive rate <= 100%
-    Srabbit* nextRabbit;
+    Srabbit* nextRabbit;  // next rabbit
 
 }Srabbit;
 
@@ -40,8 +43,8 @@ void updateStats(Srabbit* rabbit) {
     //update maturaty
     if (rabbitAge >= 5 && rabbitAge<=8) {
 
-        float randNB = genrand_real1();
-        if (randNB < (rabbitAge - 4) * 25) {
+        float randNB = genrand_real1() * 100;
+        if (randNB <= (rabbitAge - 4) * 25) {
 
             rabbit->mature = 1;
             rabbit->srvRate = 60;
@@ -58,21 +61,43 @@ void updateStats(Srabbit* rabbit) {
     }
 }
 
+int giveBirth(Srabbit*  rabbit) {
+    int prctg = 25;
+    
+    if (rabbit->sexe == 'F' && rabbit->mature == 1 && rabbit->pregnant == 0) {
+        if (rabbit->nbLitters < rabbit->nbLittersY) {
 
+            rabbit->nbLitters += 1;
+            float randNB = genrand_real1() * 100;
+            while (1){
+                if (randNB <= prctg) {
+                    return 2 + prctg / 25;
+                }
+                else {
+                    prctg += 25;
+                }
+            }
+        }
+
+    }
+}
+
+void createRabbitsList();
 
 void sim(int N) {  //number of months
     Srabbit* rabbit = malloc(sizeof(Srabbit*));
     int population = 2;
+    int newKittens;
 
     //first 2 rubbits
-    *rabbit = (Srabbit){ 'M', 1, 0, 0, 35, &(Srabbit) { 'F', 1, 0, 0, 35, NULL } };
+    *rabbit = (Srabbit){ 'M', 1, 0, 0, 0, 0, 35, &(Srabbit) { 'F', 1, 0, 0, 0, 0, 35, NULL } };
 
     for (int i = 0; i < N; i += TIME_STEP) {
         //run of all the existing rabbits
         for (int j = 0; j < population; j++) {
             if (rabbit->status != 0) {  // if not already dead
 
-
+                newKittens = giveBirth(rabbit);
                 updateStats(rabbit);
 
                 rabbit = rabbit->nextRabbit;
